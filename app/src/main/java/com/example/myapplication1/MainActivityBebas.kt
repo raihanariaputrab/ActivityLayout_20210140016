a5a89c1c1144b3a44441bac93f62ddef3c1440ef
raihanariaputrab <arpb.business@gmail.com>
2023-10-02 17:07:18 +0700

    commit 1

---
+++ app/src/main/java/com/example/myapplication1/MainActivityBebas.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivityBebas : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_bebas)
+    }
+}
