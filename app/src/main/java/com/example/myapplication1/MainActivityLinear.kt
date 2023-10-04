196c22cdfa52548dad8225c327d4471c6bf768cc
raihanariaputrab <arpb.business@gmail.com>
2023-10-04 15:57:47 +0700

    act table

--- app/src/main/java/com/example/myapplication1/MainActivityLinear.kt
+++ app/src/main/java/com/example/myapplication1/MainActivityLinear.kt
@@ -3,9 +3,9 @@ package com.example.myapplication1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity3 : AppCompatActivity() {
+class MainActivityLinear : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main3)
+        setContentView(R.layout.activity_main_linear)
     }
 }
---
+++ app/src/main/java/com/example/myapplication1/MainActivityTable.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivityTable : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_table)
+    }
+}
